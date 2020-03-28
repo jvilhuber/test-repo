86f8c91ae9dc93be0ebee348217f814a0d48a174
Jan Vilhuber <jan@vilhuber.com>
2020-03-28 15:15:45 +0700

    unbreaking the tests

--- src/hello/hello_internals_test.go
+++ src/hello/hello_internals_test.go
@@ -10,10 +10,3 @@ func TestExpectedGreeting(t *testing.T) {
        t.Errorf("Greeting string is incorrect, got: %s, want: %s.", test_greeting, expected_greeting_string)
     }
 }
-const not_expected_greeting_string = "Hello World!";
-func TestUnexpectedGreeting(t *testing.T) {
-    test_greeting := GetGreetingString();
-    if test_greeting != not_expected_greeting_string {
-       t.Errorf("Greeting string is incorrect, got: %s, want: %s.", test_greeting, not_expected_greeting_string)
-    }
-}
